6e8e185d1a1cead206105dc84630a4f5f5190e90
sungbin <21700613@handong.edu>
2022-03-14 10:35:30 +0900

    merge

--- test/make_jsondump.sh
+++ test/make_jsondump.sh
@@ -1,8 +1,6 @@
 git clone https://github.com/zserge/jsmn.git
 cd jsmn
 git checkout 6784c826d9674915a4d89649c6288e6aecb4110d
-export ASAN_OPTIONS=detect_leaks=0
-CFLAGS="-O3 -g -fsanitize=address" LDFLAGS="-fsanitize=address" make jsondump
 mv jsondump ..
 cd ..
 
