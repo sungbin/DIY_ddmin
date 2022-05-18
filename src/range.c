dac8cd84091ce18a21c606a5831df8b21787ef30
sungbin <21700613@handong.edu>
2022-05-18 15:21:02 +0900

    before grap multi-answer

--- src/range.c
+++ src/range.c
@@ -151,7 +151,7 @@ range_increasing_dir (char * program_path, char * input_dir, char * input_name, 
 	int out_fd = fileno(out_fp);
 
 	while (rs < (input_size = byte_count_file(input_path))) {
-		printf("rs: %d \n", rs);
+		//printf("rs: %d \n", rs);
 	
 		int begin = 0;
 		while (begin <= ((input_size = byte_count_file(input_path)) - rs)) {
@@ -191,7 +191,7 @@ range_increasing_dir (char * program_path, char * input_dir, char * input_name, 
 			if (file_no % 1000 == 0) {
 				time_t t = time(NULL);
 				struct tm tm = *localtime(&t);
-				fprintf(stderr, "Test count:%d -  %d-%d-%d %d:%d:%d\n", file_no, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+				fprintf(stderr, "(rs=%d)Test count:%d -  %d-%d-%d %d:%d:%d\n", rs,file_no, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 			}
 			file_no++;
@@ -220,12 +220,14 @@ range_increasing_dir (char * program_path, char * input_dir, char * input_name, 
 	return strdup(input_path);
 }
 
+int last_file_no;
 
 char *
 range (char * program_path, char * input_path, int rs, char * err_msg) {
 
 	long input_size = byte_count_file(input_path);
-	fprintf(stderr, "last minimized: %s, %ld\n", input_path, input_size);
+	fprintf(stderr, "last minimized: %s, %ld (used_test: %d)\n", input_path, input_size, file_no-last_file_no);
+	last_file_no = file_no;
 	
 	char * out_file = calloc(sizeof(char), 512);
 	sprintf(out_file, "./%d.part", ++iter_no);
@@ -234,18 +236,19 @@ range (char * program_path, char * input_path, int rs, char * err_msg) {
 	FILE * out_fp = fopen(out_file, "wb");
 	int in_fd = fileno(in_fp);
 	int out_fd = fileno(out_fp);
+	FILE * null_fp = fopen("/dev/null", "wb");
 
 	while (rs > 0) {
-		fprintf(stderr,"rs:%d\n", rs);
+		//fprintf(stderr,"rs:%d\n", rs);
 		for (int begin = 0; begin <= input_size - rs; begin++) {
+			
+			//fprintf(stderr, "rs=%d, begin=%d \n", rs, begin);
 
 			init_cursor(in_fd, out_fd);
 
 			read_and_write(in_fp, out_fp, begin); //prefix
 
-			FILE * null_fp = fopen("/dev/null", "wb");
 			read_and_write(in_fp, null_fp, rs); //rs
-			fclose(null_fp);
 
 			read_and_write(in_fp, out_fp, input_size - (begin + rs)); //postfix
 
@@ -268,22 +271,30 @@ range (char * program_path, char * input_path, int rs, char * err_msg) {
 			
 
 			int e_code = test_buffer_overflow(program_path, out_file, err_msg);
+			/*
+			if (e_code) {
+				fprintf(stderr, "rs=%d, begin=%d, e_code=%d \n", rs, begin, e_code);
+				exit(1);
+			}
+			*/
 			if (file_no % 1000 == 0) {
 				time_t t = time(NULL);
 				struct tm tm = *localtime(&t);
-				fprintf(stderr, "Test count:%d -  %d-%d-%d %d:%d:%d\n", file_no, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+				fprintf(stderr, "(rs=%d)Test count:%d -  %d-%d-%d %d:%d:%d\n", rs, file_no, tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 			}
 			file_no++;
                         if (e_code == 1) {
 				fclose(out_fp);
 				fclose(in_fp);
+				fclose(null_fp);
 				free(input_path);
 				return range(program_path, out_file, rs, err_msg);
 			}
 		}
 		rs--;
 	}
+	fclose(null_fp);
 	fclose(out_fp);
 	fclose(in_fp);
 
